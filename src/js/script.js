ba8287b0af8d10b46236aff84eb99430324ebeef
Vlad <druzhe.valdemar@gmail.com>
2022-02-17 13:40:29 +0200

    catalog

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-/* $(document).ready(function () {
+$(document).ready(function () {
     //your code here
     function toggleSlide(item) {
         $(item).each(function(i) {
@@ -12,20 +12,8 @@
     };
     toggleSlide('.button__details');
     toggleSlide('.button__collapse');
-}); */
-
-let item = {
-    button: document.getElementsByClassName('button__details')[0],
-    content: document.getElementsByClassName('courses-item__content'),
-    details: document.getElementsByClassName('courses-item__details')
-};
-item.button.addEventListener("click", function() {
-    alert("adfadsf");
 });
-/* for (var i = 0 ; i < item.button.length; i++) {
-    item.button[i].addEventListener("click", function(){
-       doc[this].classList.toggle('courses-item__content_active');
-       det[this].classList.toggle('courses-item__details_active');
-    });
- } */
+
+
+
 
